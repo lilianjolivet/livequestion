a38c0f15e63c5f93a4dd16f23e38c5f77b857d3d
Spacelocust <39099403+Spacelocust@users.noreply.github.com>
2020-03-29 18:17:57 +0200

    Delete db.php

--- LQ lucas/db/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    function connexionBdd() {
-		$user = "root";
-   		$pass = "root";
-   		$dbName = "live_question";
-   		return new \PDO("mysql:host=127.0.0.1;dbname=$dbName;charset=UTF8", $user, $pass);
-	}
-?>
