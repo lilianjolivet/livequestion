4dd1e30b13c397e5c1baa173e59614d2afe8d638
Spacelocust <39099403+Spacelocust@users.noreply.github.com>
2020-03-29 18:10:08 +0200

    Delete test.php

--- LQ laura/liveQuestion/test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-if(isset($_SESSION['utilisateur']) && !empty($_SESSION['utilisateur'])){
-     echo $_SESSION['utilisateur']['pseudo'].' '.$_SESSION['utilisateur']['id']; 
-}
-
-
-?>
